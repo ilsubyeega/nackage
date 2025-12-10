2ec2db0f9e3884524fd6168e794f329ee422e96e
ilsubyeega <git@fmt.kr>
2025-12-10 23:30:23 +0900

    bump

--- scripts/push-s3.sh
+++ scripts/push-s3.sh
@@ -1,1 +1,3 @@
+nix shell nixpkgs#awscli --command aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $AWS_PROFILE_NAME
+nix shell nixpkgs#awscli --command aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile $AWS_PROFILE_NAME
 nix copy --to s3://nackage\?profile=$AWS_PROFILE_NAME\&endpoint=$S3_API_ENDPOINT\&compression=zstd .#all
