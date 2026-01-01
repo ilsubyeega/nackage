c3de05db99e4e32e9480e6c1fdacdb3bdfa4b4f9
ilsubyeega <git@fmt.kr>
2026-01-01 15:10:02 +0900

    bump

--- scripts/push-s3.sh
+++ scripts/push-s3.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
 nix shell nixpkgs#awscli --command aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $AWS_PROFILE_NAME
 nix shell nixpkgs#awscli --command aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile $AWS_PROFILE_NAME
-nix copy --to s3://nackage\?profile=$AWS_PROFILE_NAME\&endpoint=$S3_API_ENDPOINT\&compression=zstd .#niri .#niri-debug .#xwayland-satellite
+echo "$BINARY_CACHE_SECRET_KEY" > /tmp/cache-key.sec
+nix copy --to s3://nackage\?profile=$AWS_PROFILE_NAME\&endpoint=$S3_API_ENDPOINT\&compression=zstd\&secret-key=/tmp/cache-key.sec .#niri .#niri-debug .#xwayland-satellite
