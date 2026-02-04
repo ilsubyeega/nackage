592540bd6857cbe69f3b607614e1dd2cf30b0377
ilsubyeega <git@fmt.kr>
2026-02-04 21:01:23 +0900

    remove libtelio as output

--- packages/default.nix
+++ packages/default.nix
@@ -7,7 +7,7 @@
 {
   inherit (inputs.xwayland-satellite.packages.${hostPlatform.system}) xwayland-satellite;
   inherit (inputs.nix-alien.packages.${hostPlatform.system}) nix-alien;
-  inherit (import ./nordvpn-linux/default.nix args) nordvpn-linux libtelio;
-  inherit (import ./niri/default.nix args) niri niri-debug;
+  inherit (import ./nordvpn-linux/default.nix args) nordvpn-linux;
+  inherit (import ./niri/default.nix args) niri;
   inherit (import ./wprs/default.nix args) wprs;
 }
